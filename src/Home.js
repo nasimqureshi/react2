30224613b2822362574cd0eb9ea739f90578be01
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-14 20:57:01 -0600

    commit

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,20 @@
 
 const Home = () =>{
+    let name = "Nasim";
+
+    const handleClick = () => {
+        name = "Qamar";
+    }
+    const handleClickAgain = () => {
+        console.log('hello' );
+    }
     return(
         <div className="home">
-          <h1>Home Page</h1>  
+          <h1>Home Page</h1>
+          <p>{ name }</p>
+          <button onClick={handleClick}>Click Me</button>
+          <button onClickAgain={handleClickAgain}>Click Me Again</button>
+
         </div>
     );
 }
